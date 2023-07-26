c5cdf17b62f74e5687bbf65682f44676f4a0c7a1
hunaidisar <37739827+hunaidisar@users.noreply.github.com>
2023-07-26 08:27:44 +0300

    Update coinFlipLoop copy.py
    + a 3 sided coin randomizer...
    + quantum choice (one of three choices; X , I , H)
    + a loop for n number of turns
    
    problems:
    100 trials loop needs changing
    need to reset the circuit at the top of each loop

--- coinFlipLoop copy.py
+++ coinFlipLoop copy.py
@@ -1,19 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 00:24:09 2023
+
+@author: Acer
+"""
+
 from qiskit import QuantumCircuit, execute,Aer
 from qiskit.visualization import plot_histogram
 import random
 qc = QuantumCircuit(1, 1)
 
+#Three Sided Coin Randomizer
 
+#first, a dumb one:
+def numpyrandomizer():
+     r2 = random.randint(0, 4)
+     return r2
+
+        
+# Then a real one..still under development, please refer to the ThreeSidedCoin py file        
+def TSCrandomizer():
+    rqc = QuantumCircuit(3,3)
+    rqc.h(0)
+    rqc.h(2)
+    rqc.ccx(0,2,1)
+    rqc.barrier(0,1,2)
+    
+    #we don't use measure_all() because index 1 must be measured first (the order of the measurement matters)  
+    rqc.measure(1,1)
+    
+    rqc.ccx(0,1,2)
+    #measure the target
+    rqc.measure(2,2)
+    
+    rqc.ccx(2,1,0)
+    #measure the target
+    rqc.measure(0,0)
+    
+    job = execute(rqc, Aer.get_backend('qasm_simulator'))
+
+    counts = job.result().get_counts()
+    r2 = list(counts) 
+    return r2
 
 def random_choice():
     qcc = QuantumCircuit(1,1)
     qcc.h(0)
     qcc.measure(0,0)
-    job = execute(qcc, Aer.get_backend('qasm_simulator'), shots=1024)
+    job = execute(qcc, Aer.get_backend('qasm_simulator'))
 
     counts = job.result().get_counts()
-    x = list(counts)[0]
-    return x
+    r1 = list(counts)[0]
+    return r1
 
 
 def Choice(true_choice):
@@ -21,20 +59,36 @@ def Choice(true_choice):
      if true_choice == 1:
         qc.x(0)
         
-       
      else :
         qc.i(0)
         
-def flip_coin():
+def QuantumChoice(true_choice):
+     
+     if true_choice == 1:
+        qc.x(0)              
+     elif true_choice == 0 :
+        qc.i(0)
+     elif true_choice == 2 :
+        qc.h(0)        
+        
+def flip_coin(n):
 
+    # #previously H
+    # QuantumChoice(numpyrandomizer())
 
-    qc.h(0)
-
-    Choice(random_choice())
+    # Choice(random_choice())
   
-
-    qc.h(0)
+    # #previously H
+    # QuantumChoice(numpyrandomizer())
     
+    #for n number of turns
+    for i in range(1,n+1):
+        
+        if i%2 == 0 :
+            
+            Choice(random_choice())
+            
+        else: QuantumChoice(numpyrandomizer())
     
 
     qc.measure(0, 0)
@@ -43,6 +97,7 @@ def flip_coin():
     job = execute(qc, Aer.get_backend('qasm_simulator'), shots=1024)
 
     counts = job.result().get_counts()
+    #print(qc)
     if track:
         print(counts)
 
@@ -57,10 +112,12 @@ track = False
 count_Head = 0
 count_Tail = 0
 
+# problem:this for loop is consecutivly applying flip on the same circuit.
+
 for i in range(0,100):
     
 
-    bit = flip_coin()
+    bit = flip_coin(7)
     if bit == 0:
         count_Head += 1
     else:
